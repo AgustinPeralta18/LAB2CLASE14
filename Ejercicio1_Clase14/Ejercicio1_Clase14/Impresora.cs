70a44b16f0bd9b26a5d7ba0697fe5afc8c4b8aac
AgustinPeralta18 <81267543+AgustinPeralta18@users.noreply.github.com>
2021-10-15 13:19:49 -0300

    clase 14

---
+++ Ejercicio1_Clase14/Ejercicio1_Clase14/Impresora.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1_Clase14
+{
+    class Impresora
+    {
+
+        List<Imprimible> imprimir = new List<Imprimible>();
+        public void imprimirTodo()
+        {
+            foreach (Imprimible mostrar in imprimir)
+            {
+                mostrar.imprimir();
+            }
+
+
+        }
+
+        public void agregarImprimible()
+        {
+            Contrato contrato = new Contrato();
+            imprimir.Add(contrato);
+
+            Foto foto = new Foto();
+            imprimir.Add(foto);
+
+            Documento documento = new Documento();
+            imprimir.Add(documento);
+        }
+
+
+
+
+    }
+}
